bedf5644e025cfde1c8f99079157e15835fb8ade
miftahif <miftachulfawaid30@gmail.com>
2022-07-04 14:33:09 +0700

    ganti halo ke hai

--- lib/main.dart
+++ lib/main.dart
@@ -74,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: const Text(
-          "haloo",
+          "Hai",
           style: TextStyle(color: Colors.black),
         ),
       ),
